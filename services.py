de6fe0a2219ab34cc27b5ff44e1fa9b67d97b3c3
Arthur Mello <arthur.bmello@gmail.com>
2024-05-25 05:55:58 +0000

    update embedding part

--- services.py
+++ services.py
@@ -1,8 +1,8 @@
 from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader, TextLoader
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import Chroma
-from langchain_community.document_loaders import PyPDFLoader, Docx2txtLoader, TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain_experimental.text_splitter import SemanticChunker
 from langchain.chains import RetrievalQA
 import tiktoken
 import os
@@ -10,6 +10,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 openai_api_key=os.environ['OPENAI_API_KEY']
+embedding_model = "text-embedding-3-large"
+embeddings = OpenAIEmbeddings(
+        model=embedding_model, openai_api_key=openai_api_key
+    )
 
 # loading PDF, DOCX and TXT files as LangChain Documents
 def load_document(file):
@@ -18,63 +22,82 @@ def load_document(file):
     name, extension = os.path.splitext(file)
 
     if extension == ".pdf":
-        print(f"Loading {file}")
         loader = PyPDFLoader(file)
     elif extension == ".docx":
-        print(f"Loading {file}")
         loader = Docx2txtLoader(file)
     elif extension == ".txt":
         loader = TextLoader(file)
     else:
         print("Document format is not supported!")
         return None
-
+    
+    print(f"Loading {file}")
     data = loader.load()
+    for doc in data:
+        doc.page_content = doc.page_content.replace('\n', ' ')
     return data
 
 
 # splitting data in chunks
-def chunk_data(data, chunk_size=256, chunk_overlap=20):
+def chunk_data(data, chunk_size=1024, chunk_overlap=100):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=chunk_size, chunk_overlap=chunk_overlap
+        chunk_size=chunk_size, chunk_overlap=chunk_overlap,
+        separators=[". ", ", ", " "]
     )
     chunks = text_splitter.split_documents(data)
     return chunks
 
 
 # create embeddings using OpenAIEmbeddings() and save them in a Chroma vector store
-def create_embeddings(chunks):
-    embeddings = OpenAIEmbeddings(
-        model="text-embedding-3-small", dimensions=1536, openai_api_key=openai_api_key
-    )  # 512 works as well
-    # vector_store = Chroma.from_documents(chunks, embeddings)
+def create_embeddings(chunks, persist_directory="./mychroma_db"):
 
+    # reset database
+    import shutil
+
+    if os.path.exists(persist_directory):
+        if os.path.isdir(persist_directory):
+            shutil.rmtree(persist_directory)
+    
     # if you want to use a specific directory for chromadb
     vector_store = Chroma.from_documents(
-        chunks, embeddings, persist_directory="./mychroma_db"
+        chunks, embeddings, persist_directory=persist_directory
     )
     return vector_store
 
+def format_sources(source_documents):
+    formatted_sources = '\n'.join(
+        [
+        f"\"{doc.page_content}\"\
+        \nFile:{doc.metadata['source'].split('/')[-1]}\
+        \nPage:{doc.metadata['page']}"
+        for doc in source_documents
+        ]
+    )
+    return formatted_sources
 
-def ask_and_get_answer(vector_store, q, k=3):
+def ask_and_get_answer(vector_store, q, k=10):
 
     llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=1)
     retriever = vector_store.as_retriever(
-        search_type="similarity", search_kwargs={"k": k}
+        search_type="mmr", search_kwargs={"k": k}
     )
     chain = RetrievalQA.from_chain_type(
-        llm=llm, chain_type="stuff", retriever=retriever
+        llm=llm, chain_type="stuff", retriever=retriever,
+        chain_type_kwargs={"verbose": True},
+        return_source_documents=True
     )
-
     answer = chain.invoke(q)
-    return answer["result"]
+
+    sources = format_sources(answer["source_documents"])
+    result = f"{answer['result']}\nSources: {sources}"
+    return result
 
 
 # calculate embedding cost using tiktoken
 def calculate_embedding_cost(texts):
-    enc = tiktoken.encoding_for_model("text-embedding-3-small")
+    enc = tiktoken.encoding_for_model(embedding_model)
     total_tokens = sum([len(enc.encode(page.page_content)) for page in texts])
     # check prices here: https://openai.com/pricing
-    # print(f'Total Tokens: {total_tokens}')
-    # print(f'Embedding Cost in USD: {total_tokens / 1000 * 0.00002:.6f}')
-    return total_tokens, total_tokens / 1000 * 0.00002
+    print(f'Total Tokens: {total_tokens}')
+    print(f'Embedding Cost in USD: {total_tokens / 1000 * 0.00002:.6f}')
+    return total_tokens, total_tokens / 1000 * 0.00013
