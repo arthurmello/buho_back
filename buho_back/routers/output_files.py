c32a735967761ab88bfdbc48ebc1a3ada2236e5b
arthurmello <arthur.bmello@gmail.com>
2024-06-27 06:13:11 +0200

    update request format and reduce template for tests

--- buho_back/routers/output_files.py
+++ buho_back/routers/output_files.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from fastapi.responses import FileResponse
 from buho_back.config import settings
+from buho_back.models import OutputFileRequest
 from buho_back.services.file_generation import generate_file
 import time
 
@@ -12,8 +13,9 @@ router = APIRouter()
 
 
 @router.post("/generate")
-async def generate_output_file(filename: str, user_id: str = "user"):
+async def generate_output_file(body: OutputFileRequest, user_id: str = "user"):
     start_time = time.time()
+    filename = body.filename
     output_file_path = generate_file(filename, user_id)
 
     end_time = time.time()
