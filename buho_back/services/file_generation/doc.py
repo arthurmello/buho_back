d5548806fb38fd65c7cfa78209388033b5f43ca0
amello <arthur_bmello@hotmail.com>
2024-07-19 12:07:40 +0200

    improve document generation and print cpu count

--- buho_back/services/file_generation/doc.py
+++ buho_back/services/file_generation/doc.py
@@ -1,12 +1,23 @@
 from markdown_pdf import MarkdownPdf, Section
 from pdf2docx import Converter
 import os
+from docx import Document
+
+
+def remove_section_breaks(doc_file_path):
+    document = Document(doc_file_path)
+    for i, section in enumerate(document.sections):
+        if i > 0:  # Skip the first section
+            section.start_type = 0  # Set section break type to continuous
+    document.save(doc_file_path)
 
 
 def generate_doc(section_contents, user_output_files_directory, filename):
     pdf = MarkdownPdf(toc_level=2)
-    for section_content in section_contents:
-        pdf.add_section(Section(f"{section_content}"))
+
+    combined_content = "\n\n".join(section_contents)
+    print(combined_content)
+    pdf.add_section(Section(combined_content))
 
     if not os.path.exists(user_output_files_directory):
         os.makedirs(user_output_files_directory)
@@ -20,4 +31,6 @@ def generate_doc(section_contents, user_output_files_directory, filename):
     doc_file_path = pdf_file_path.replace(".pdf", ".docx")
     cv.convert(doc_file_path)
     cv.close()
+
+    remove_section_breaks(doc_file_path)
     return doc_file_path
