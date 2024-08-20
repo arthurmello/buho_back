dd8298367e6303760890d7b27a2090040c7f70b2
Arthur Mello <arthur.bmello@gmail.com>
2024-08-20 16:46:29 +0200

    update retriever to use hybrid

--- buho_back/services/storage/vectordb.py
+++ buho_back/services/storage/vectordb.py
@@ -24,17 +24,32 @@ class VectorDbClient:
             name=self.collection_name, embedding_function=self.embedding_function
         )
 
-        def retrieve_chunks(text, k=10, score_threshold=-0.1):
+        def retrieve_chunks(text, k=3, distance_threshold=None):
+            first_k = 10
             query_results = collection.query(
                 query_texts=text,
-                n_results=k,
+                n_results=first_k,
             )
             formatted_query_results = query_result_to_dict_list(query_results)
-            if score_threshold:
+
+            for d in formatted_query_results:
+                similarity = jaccard_similarity(text, d.get("document", ""))
+                d["jaccard_similarity"] = similarity
+                d["semantic_similarity"] = 1 / d["distance"]
+                d["hybrid_similarity"] = (
+                    d["jaccard_similarity"] * d["semantic_similarity"]
+                )
+
+            formatted_query_results.sort(
+                key=lambda x: x["hybrid_similarity"], reverse=True
+            )
+            formatted_query_results = formatted_query_results[:k]
+
+            if distance_threshold:
                 filtered_formatted_query_results = [
                     result
                     for result in formatted_query_results
-                    if result["distance"] > score_threshold
+                    if result["distance"] < distance_threshold
                 ]
                 return filtered_formatted_query_results
             else:
@@ -63,23 +78,31 @@ def query_result_to_dict_list(
 ) -> List[Dict[str, Optional[str]]]:
     result_list = []
 
-    ids = query_result.get("ids", [])
-    metadatas = query_result.get("metadatas", [])
-    documents = query_result.get("documents", [])
-    distances = query_result.get("distances", [])
+    ids = query_result.get("ids", [])[0]
+    metadatas = query_result.get("metadatas", [])[0]
+    documents = query_result.get("documents", [])[0]
+    distances = query_result.get("distances", [])[0]
 
     for i in range(len(ids)):
         result_dict = {
-            "id": ids[i][0] if i < len(ids) and ids[i] else None,
-            "metadata": (
-                metadatas[i][0] if i < len(metadatas) and metadatas[i] else None
-            ),
-            "document": (
-                documents[i][0] if i < len(documents) and documents[i] else None
-            ),
-            "distance": (
-                distances[i][0] if i < len(distances) and distances[i] else None
-            ),
+            "id": ids[i] if i < len(ids) and ids[i] else None,
+            "metadata": (metadatas[i] if i < len(metadatas) and metadatas[i] else None),
+            "document": (documents[i] if i < len(documents) and documents[i] else None),
+            "distance": (distances[i] if i < len(distances) and distances[i] else None),
         }
         result_list.append(result_dict)
     return result_list
+
+
+def jaccard_similarity(str1, str2):
+    words1 = set(str1.lower().split())
+    words2 = set(str2.lower().split())
+
+    intersection = words1.intersection(words2)
+    union = words1.union(words2)
+
+    if not union:
+        return 0.0  # Avoid division by zero
+    similarity = len(intersection) / len(union)
+
+    return similarity
